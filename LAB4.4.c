2cc96c12a295c8b5fefe1385671c7215fbbf4094
iubireStoned <101269959+iubireStoned@users.noreply.github.com>
2022-03-09 19:41:40 +0300

    pointer

---
+++ LAB4.4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	char str[4]={'a','b','c','d'};
+    char *p1;
+    int i;
+    
+    p1=str;
+	
+	for(i=0;p1[i]!='\0' && i<4 ;i++){
+		
+	}
+	printf("%d",i);	
+}
